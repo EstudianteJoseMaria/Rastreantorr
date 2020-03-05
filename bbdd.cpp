0865f563bb27131045804af24305491f66449316
usuario <usuario@linux-dpma.edib.es>
2020-03-05 12:24:20 +0100

    Entrega

--- bbdd.cpp
+++ bbdd.cpp
@@ -28,32 +28,37 @@ bool BBDD::open()
 void BBDD::leerArchivo(std::string archivo)
 {
     std::ifstream fin(archivo);
-    std::string line;
-    std::istringstream sin;
-    while (std::getline(fin, line)) {
-     sin.str(line.substr(line.find(":")+1));
-     if (line.find("host") != std::string::npos) {
-      std::cout<<"host "<<sin.str()<<std::endl;
-      m_db.setHostName(QString::fromStdString(sin.str()));
-     }
-     else if (line.find("port") != std::string::npos) {
-      //std::cout<<"port "<<sin.str()<<std::endl;
-      m_db.setPort(std::stoi(sin.str()));
-     }
-     else if (line.find("user") != std::string::npos) {
-      //std::cout<<"user "<<sin.str()<<std::endl;
-      m_db.setUserName(QString::fromStdString(sin.str()));
-     }
-     else if (line.find("password") != std::string::npos) {
-      //std::cout<<"pass "<<sin.str()<<std::endl;
-      m_db.setPassword(QString::fromStdString(sin.str()));
-     }
-     else if (line.find("database") != std::string::npos) {
-      //std::cout<<"pass "<<sin.str()<<std::endl;
-      m_db.setDatabaseName(QString::fromStdString(sin.str()));
-     }
-     sin.clear();
-    }
+    if(fin.is_open())
+    {
+        qDebug() << "Config correcto";
+        std::string line;
+        std::istringstream sin;
+        while (std::getline(fin, line)) {
+            sin.str(line.substr(line.find(":")+1));
+            if (line.find("host") != std::string::npos) {
+             std::cout<<"host: "<<sin.str()<<std::endl;
+             m_db.setHostName(QString::fromStdString(sin.str()));
+            } //endif
+            else if (line.find("port") != std::string::npos) {
+             //std::cout<<"port "<<sin.str()<<std::endl;
+             m_db.setPort(std::stoi(sin.str()));
+            } // endif
+            else if (line.find("user") != std::string::npos) {
+             //std::cout<<"user "<<sin.str()<<std::endl;
+             m_db.setUserName(QString::fromStdString(sin.str()));
+            } // endif
+            else if (line.find("password") != std::string::npos) {
+             //std::cout<<"pass "<<sin.str()<<std::endl;
+             m_db.setPassword(QString::fromStdString(sin.str()));
+            } // endif
+            else if (line.find("database") != std::string::npos) {
+             //std::cout<<"pass "<<sin.str()<<std::endl;
+             m_db.setDatabaseName(QString::fromStdString(sin.str()));
+            } // endif
+            sin.clear();
+        }
+    } // endif
+    else qDebug() << "No se ha encontrado el archivo";
 }
 
 BBDD::BBDD(std::string rutaArchivo)
